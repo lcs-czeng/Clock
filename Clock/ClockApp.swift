d9b5b5c1b5710bd1e5325f59d99028598aa4eb1b
Courage Zeng <czeng@lcs.on.ca>
2024-11-08 08:50:51 -0500

    Initial Commit

---
+++ Clock/ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockApp.swift
+//  Clock
+//
+//  Created by 曾梓恒 on 2024/11/8.
+//
+
+import SwiftUI
+
+@main
+struct ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
